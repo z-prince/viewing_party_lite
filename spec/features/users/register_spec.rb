93f9c834de177451913d4cf0ebdfda4f25ddbebe
Zachary P <zachary.s.prince@outlook.com>
2022-07-18 17:40:05 -0400

    Accidentally modified this test

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe 'user registration page' do
 
     expect(current_path).to eq("/users/#{User.last.id}")
   end
+
+  it 'returns a user to the register page if they miss forms' do
+    visit '/register'
+
+    fill_in('Name', with: 'Sylvester Tommy')
+    fill_in('Password', with: 'hellcats420')
+    fill_in('Password Confirmation', with: 'hellcats420')
+
+    click_on('Register')
+
+    expect(current_path).to eq('/register')
+
+    expect(page).to have_content('Missing Required Fields')
+  end
 end
