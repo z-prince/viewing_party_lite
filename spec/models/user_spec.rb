95e830b94046e8a73a5ce9d93ac74748918abfa3
Zachary P <zachary.s.prince@outlook.com>
2022-07-18 17:39:18 -0400

    Add model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
+    it { should have_secure_password }
     it { should have_many(:party_users) }
     it { should have_many(:parties).through(:party_users) }
   end
