99005ae75ca59c059f6bbb9c49e1591eb8613909
Zachary P <zachary.s.prince@outlook.com>
2022-07-18 13:44:16 -0400

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   validates_presence_of  :name
   validates_presence_of  :email
   validates_uniqueness_of :email
+  validates_presence_of :password
   has_many :party_users
   has_many :parties, through: :party_users
+
+  has_secure_password
 
   def host?(party_id)
     party_users.where(party_id: party_id).first.host
