7ceb2eeedb4599ae52b28f82ef1dbabc478591c4
Zachary P <zachary.s.prince@outlook.com>
2022-07-18 13:36:01 -0400

    Add user registration test

---
+++ spec/features/register_user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User registration form' do
+  it 'creates new user' do
+    visit root_path
+
+    click_on 'Register as a User'
+
+    expect(current_path).to eq(new_user_path)
+
+    email = 'happy@me.com'
+    password = 'test'
+    name = 'Sandy Sarco'
+
+    fill_in :user_email, with: email
+    fill_in :user_name, with: name
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password
+
+    click_on 'Create User'
+
+    expect(page).to have_content("Welcome, #{email}!")
+  end
+end
