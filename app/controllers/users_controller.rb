9f86dd81b0df78793aa3edfd6fb796edbc490a37
Zachary P <zachary.s.prince@outlook.com>
2022-07-18 17:40:33 -0400

    Add login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,36 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    new_user = User.create!(user_params)
-
-    redirect_to user_path(id: new_user.id)
+    new_user = User.new(user_params)
+    if params[:password] != params[:password_confirmation]
+      flash[:error] = 'Password and confirmation do not match!'
+      redirect_to '/register'
+    elsif new_user.save
+      redirect_to user_path(id: new_user.id)
+    else
+      flash[:error] = 'Missing Required Fields'
+      redirect_to '/register'
+    end
   end
 
   def discover
     @user = User.find(params[:id])
   end
 
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      redirect_to user_path(id: user.id)
+    else
+      flash[:error] = 'Wrong email or password'
+      redirect_to '/login'
+    end
+  end
+
   private
 
   def user_params
-    params.permit(:email, :name)
+    params.permit(:name, :password, :email)
   end
 end
